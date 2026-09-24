8581f7ba822094f8fa985fd2901236002ddc6658
Ajay Panchal <ajaypanch@gmail.com>
2026-09-24 13:25:39 +0100

    Make scroll reveal fail-safe

--- site.js
+++ site.js
@@ -17,6 +17,7 @@
   window.addEventListener('scroll',onScroll,{passive:true});onScroll();
   var els=document.querySelectorAll('.rv');
   if('IntersectionObserver' in window){
+    root.classList.add('js-rv');
     var io=new IntersectionObserver(function(es){es.forEach(function(e){if(e.isIntersecting){e.target.classList.add('in');io.unobserve(e.target);}});},{rootMargin:'0px 0px -8% 0px'});
     els.forEach(function(el){io.observe(el);});
   }else{els.forEach(function(el){el.classList.add('in');});}
